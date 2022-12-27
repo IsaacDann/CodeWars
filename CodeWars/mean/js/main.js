dfcfac605d6a3e2fa7d90226c5b70b8fce9036f0
Isaac <isaaccdann@gmail.com>
2022-12-27 13:28:07 -0800

    sortOdds

--- CodeWars/mean/js/main.js
+++
@@ -1,4 +0,0 @@
-var findAverage = function (nums) {
-    let sum = nums.reduce((a, c) => a + c, 0)
-    return sum/nums.length
-  }
--- CodeWars/sumOf2Lowest/js/main.js
+++
@@ -1,7 +0,0 @@
-// Given an array, create a function that returns the sum of the two lowest integers.
-
-
-function sumTwoSmallestNumbers(numbers) {  
-  let sorted = numbers.sort((a, b) => a- b)
-  return sorted[0] + sorted[1]
-}
