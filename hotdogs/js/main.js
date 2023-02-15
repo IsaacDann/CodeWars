1ea99e6d6d25e03f45ea43d003ad3f23470c636e
IsaacDann <isaaccdann@gmail.com>
2023-02-15 13:25:53 -0700

    8kyu

---
+++ hotdogs/js/main.js
@@ -0,0 +1,17 @@
+// Task:
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+// number of hotdogs	price per unit (cents)
+// n < 5	100
+// n >= 5 and n < 10	95
+// n >= 10	90
+
+function saleHotdogs(n){
+    if(n >= 10){
+      return 90*n
+    }else if(n < 10 && n >= 5){
+      return 95* n
+    }else{
+      return 100 * n
+    }
+  }
